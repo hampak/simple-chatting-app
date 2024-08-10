9f017fd315729223a4833eebb5cd117940b1a66e
Haemin Park <haemin.parque@gmail.com>
2024-08-10 20:29:17 +0900

    first commit

---
+++ server/dist/src/index.js
@@ -0,0 +1,46 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const cors_1 = __importDefault(require("cors"));
+const helmet_1 = __importDefault(require("helmet"));
+const morgan_1 = __importDefault(require("morgan"));
+// route imports
+// import dashboardRoutes from "./routes/dashboardRoutes"
+// configurations
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use((0, helmet_1.default)());
+app.use(helmet_1.default.crossOriginResourcePolicy({
+    policy: "cross-origin"
+}));
+app.use((0, morgan_1.default)("common"));
+app.use(body_parser_1.default.json());
+app.use(body_parser_1.default.urlencoded({ extended: false }));
+app.use((0, cors_1.default)());
+// routes
+// app.use("/dashboard", dashboardRoutes) // http://localhost:8000/dashboard
+app.get("/hello", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    res.send({
+        message: "hello"
+    });
+}));
+// server
+const port = Number(process.env.PORT) || 3001;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
