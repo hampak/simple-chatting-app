b7d5b48ef59609bd8d5d0c7abdeda0f05cf8d46a
Haemin Park <haemin.parque@gmail.com>
2024-08-11 16:45:08 +0900

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,16 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // server: {
-  //   proxy: {
-  //     port: "8000",
-  //     "/socket.io/": {
-  //       target: "http://localhost:8000",
-  //       changeOrigin: true,
-  //       secure: false,
-  //       ws: true
-  //     }
-  //   }
-  // },
   plugins: [react()],
 })
