6706c415210761af4463756034973838d7353af5
Haemin Park <haemin.parque@gmail.com>
2024-08-11 16:33:10 +0900

    Update io.ts

--- client/src/utils/io.ts
+++ client/src/utils/io.ts
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-const uri = import.meta.env.VITE_STATUS === "development" ? "http://localhost:8000" : "https://server-production-982c.up.railway.app/"
+// const uri = import.meta.env.VITE_STATUS === "development" ? "http://localhost:8000" : "https://server-production-982c.up.railway.app/"
 
-export const socket = io(uri)
+export const socket = io("https://server-production-982c.up.railway.app/")
 // export const socket = io(undefined)
